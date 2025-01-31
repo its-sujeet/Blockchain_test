d56cba91fb67b7596229f39beaeaef3069d0cf96
its-sujeet <sujeetkumar7846540@gmail.com>
2025-02-01 01:40:40 +0530

    created struct

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.8;
 contract SimpleStorage{
         uint256 public  favnum;
 
-        // struct people 
+         struct people {
+            uint256 favnum;
+            string name;
+         }
+
+         people public suj = people({favnum : 75, name : "Arush"});
 
         function store (uint256 _favnum) public {
             favnum = _favnum;
